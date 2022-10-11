c3c5f0ef3e542c90f138a07d847f72425ffd88c1
Max <max@refactory.me>
2022-10-11 09:13:38 +0800

    Generate images for CMI Aug 2022

--- nft_generate.py
+++ nft_generate.py
@@ -77,7 +77,8 @@ def generate_single_image(filepaths, output_filename=None, compression_ratio = 1
     for filepath in filepaths[1:]:
         if filepath.endswith('.png'):
             img = Image.open(os.path.join(parent_dir, 'assets', filepath))
-            bg.paste(img, (0,0), img)
+            # bg.paste(img, (0,0), img)
+            bg.paste(img.convert('RGB'), (0,0), img)
 
     if compression_ratio != 1:
         width, height = bg.size
@@ -86,6 +87,8 @@ def generate_single_image(filepaths, output_filename=None, compression_ratio = 1
         newHeight = int(height * compression_ratio)
         bg = bg.resize((newWidth, newHeight))
 
+    bg = bg.convert('RGB')
+
     # Save the final image into desired location
     if output_filename is not None:
         bg.save(output_filename)
@@ -93,7 +96,7 @@ def generate_single_image(filepaths, output_filename=None, compression_ratio = 1
         # If output filename is not specified, use timestamp to name the image and save it in output/single_images
         if not os.path.exists(os.path.join('output', 'single_images')):
             os.makedirs(os.path.join('output', 'single_images'))
-        bg.save(os.path.join('output', 'single_images', str(int(time.time())) + '.png'))
+        bg.save(os.path.join('output', 'single_images', str(int(time.time())) + '.jpg'))
 
 
 # Generate a single image with all possible traits
@@ -233,11 +236,14 @@ def generate_images(edition, count, brand_name, is_random, compression_ratio, dr
     
     else:
         # BIG ASSUMPTION!! Assume there are always 5 layers.
-        LAYER_1_COUNT = 2
-        LAYER_2_COUNT = 1
+        LAYER_1_COUNT = 4
+        LAYER_2_COUNT = 5
         LAYER_3_COUNT = 5
         LAYER_4_COUNT = 5
-        LAYER_5_COUNT = 1
+        LAYER_5_COUNT = 4
+
+        current_index = 0
+        selected_indexes = [20, 21, 23, 24, 25, 27, 28, 29, 31, 32, 33, 35, 36, 37, 39, 80, 84, 88, 92, 96, 100, 104, 108, 109, 112, 113, 116, 117, 120, 121, 123, 124, 125, 127, 128, 129, 131, 132, 133, 135, 136, 137, 139, 140, 142, 144, 146, 148, 150, 152, 154, 156, 158, 160, 161, 162, 164, 166, 168, 169, 170, 172, 174, 176, 177, 178, 281, 282, 285, 286, 289, 290, 293, 294, 297, 298, 301, 302, 305, 306, 309, 310, 313, 314, 317, 318, 382, 383, 386, 387, 390, 391, 394, 398, 600, 603, 604, 607, 608, 611, 612, 615, 616, 619, 620, 621, 622, 623, 624, 625, 626, 627, 628, 629, 630, 631, 632, 633, 634, 635, 636, 637, 638, 639, 642, 646, 650, 654, 658, 702, 703, 706, 707, 710, 711, 714, 715, 718, 719, 722, 726, 730, 734, 738, 741, 745, 749, 753, 757, 821, 825, 829, 833, 837, 921, 925, 929, 933, 937, 940, 944, 948, 952, 956, 1102, 1106, 1110, 1114, 1118, 1121, 1122, 1123, 1125, 1126, 1127, 1129, 1130, 1131, 1133, 1134, 1135, 1137, 1138, 1139, 1143, 1147, 1151, 1155, 1159, 1222, 1226, 1230, 1234, 1238, 1260, 1263, 1264, 1268, 1271, 1272, 1275, 1276, 1279, 1280, 1281, 1283, 1284, 1285, 1287, 1288, 1289, 1291, 1292, 1293, 1295, 1296, 1297, 1299, 1300, 1303, 1304, 1308, 1311, 1312, 1316, 1319, 1360, 1362, 1364, 1366, 1367, 1368, 1370, 1372, 1374, 1375, 1376, 1378, 1379, 1407, 1415, 1420, 1421, 1424, 1425, 1428, 1429, 1432, 1433, 1436, 1437, 1471, 1480, 1482, 1483, 1484, 1486, 1487, 1488, 1490, 1491, 1492, 1494, 1495, 1496, 1498, 1623, 1627, 1631, 1635, 1639, 1661, 1663, 1665, 1667, 1669, 1671, 1673, 1675, 1677, 1679, 1720, 1722, 1724, 1726, 1728, 1730, 1732, 1734, 1736, 1738, 1741, 1743, 1745, 1747, 1749, 1751, 1753, 1755, 1757, 1759, 1765, 1773, 1801, 1806, 1809, 1814, 1817, 1882, 1883, 1885, 1887, 1889, 1890, 1891, 1893, 1895, 1897, 1898, 1899]
 
         bar = progressbar.ProgressBar(max_value=count)
         for i1 in range(LAYER_1_COUNT):
@@ -246,24 +252,30 @@ def generate_images(edition, count, brand_name, is_random, compression_ratio, dr
                     for i4 in range(LAYER_4_COUNT):
                         for i5 in range(LAYER_5_COUNT):
 
-                            # Set image name
-                            image_name = str(generated_count).zfill(zfill_count) + '.png'
+                            if current_index in selected_indexes:
 
-                            # Get a random set of valid traits based on rarity weights
-                            trait_sets, trait_paths = generate_predefined_trait_set_from_config([i1, i2, i3, i4, i5])
+                                # Set image name
+                                image_name = str(current_index).zfill(zfill_count) + '.jpg'
 
-                            # Generate the actual image
-                            generate_single_image(trait_paths, os.path.join(op_path, image_name), compression_ratio)
+                                # Get a random set of valid traits based on rarity weights
+                                trait_sets, trait_paths = generate_predefined_trait_set_from_config([i1, i2, i3, i4, i5])
 
-                            # Populate the rarity table with metadata of newly created image
-                            for idx, trait in enumerate(trait_sets):
-                                if trait is not None:
-                                    rarity_table[CONFIG[idx]['name']].append(trait[: -1 * len('.png')])
-                                else:
-                                    rarity_table[CONFIG[idx]['name']].append('none')
-                            
-                            generated_count = generated_count + 1
-                            bar.update(generated_count)
+                                # Generate the actual image
+                                generate_single_image(trait_paths, os.path.join(op_path, image_name), compression_ratio)
+
+                                # Populate the rarity table with metadata of newly created image
+                                for idx, trait in enumerate(trait_sets):
+                                    if trait is not None:
+                                        rarity_table[CONFIG[idx]['name']].append(trait[: -1 * len('.png')])
+                                    else:
+                                        rarity_table[CONFIG[idx]['name']].append('none')
+
+                            else:
+                                print(f'{current_index} is not selected')
+                        
+                            current_index = current_index + 1
+                            bar.update(current_index)
+
 
     # Create the final rarity table by removing duplicate creat
     rarity_table = pd.DataFrame(rarity_table).drop_duplicates()
@@ -291,7 +303,7 @@ def generate_images(edition, count, brand_name, is_random, compression_ratio, dr
     return rarity_table
 
 # Main function. Point of entry
-def main(num_of_img, name_of_edition, name_of_brand, is_random, compression_ratio):
+def main(num_of_img, name_of_edition, name_of_brand, is_random, compression_ratio, de_dup):
 
     print("Checking assets...")
     parse_config()
@@ -314,7 +326,7 @@ def main(num_of_img, name_of_edition, name_of_brand, is_random, compression_rati
     edition_name = name_of_edition
     num_avatars = int(num_of_img)
     print("Starting task...")
-    rt = generate_images(edition_name, num_avatars, name_of_brand, is_random, compression_ratio)
+    rt = generate_images(edition_name, num_avatars, name_of_brand, is_random, compression_ratio, de_dup)
 
     print("Saving metadata...")
     rt.to_csv(os.path.join('static', 'output', 'edition_' +  str(edition_name), 'metadata.csv'))
@@ -323,4 +335,4 @@ def main(num_of_img, name_of_edition, name_of_brand, is_random, compression_rati
 
 
 # Run the main function
-# main(50, "v8_rare", "amuro", False, 0.5)
+main(2500, "v9_1_normal_final", "cmi", False, 1, False)
